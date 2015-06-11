b5f116b69fd8df845a23af1ce345bb80b5410bb3
Joren <polfliet.joren@gmail.com>
2015-06-11 15:59:46 +0200

    Login mid fixed

--- index.php
+++ index.php
@@ -116,5 +116,4 @@ if(isset($_POST['favorite_row']))
     
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
     <script src="js/menu.js" type="text/javascript"></script>
-
 </html>
